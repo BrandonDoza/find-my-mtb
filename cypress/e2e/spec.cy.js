82d089aec24d4016bc5e0333033514f5d4387f15
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-06-02 12:52:10 -0600

    add cypress testing to api call and intercept/stub response

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,16 +1,15 @@
-// describe('template spec', () => {
-//   it('passes', () => {
-//     cy.visit('https://example.cypress.io')
-//   })
-// })
-describe('Home Page', () => 
-it('Succesfully Loads', () => {
-  cy.visit(' http://localhost:3000')
-}))
-describe('Header component', () => {
-  it('Should have a page title and navigation links', () => {
+describe('Home Page', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000');
-
+    cy.intercept('GET', 'http://localhost:3001/api/v1/bikes', {
+      statusCode: 200,
+      fixture: './mockdata.json'
+    });
+  });
+  it('Successfully Loads', () => {
+    cy.visit('http://localhost:3000');
+  });
+  it('Should have a page title and navigation links', () => {
     cy.get('.header').contains('Find My Mtb');
 
     cy.get('.nav-bar').within(() => {
