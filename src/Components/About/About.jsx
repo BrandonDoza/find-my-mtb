1b8191b02eb48663adbf6cf1bccb8a785f9686c9
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-06-03 13:00:18 -0600

    fix typo in about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
         <div className="about-card">
         <section className="about-img">
             <div className="paragraph">
-                <p>We're all about helping you find the perfect bike that fits your needs. Whether you're new to the sport or a seasoned pro, you've come to the right place!</p>
+                <p>We're all about helping you find the perfect bike to fit your needs. Whether you're new to the sport or a seasoned pro, you've come to the right place!</p>
             </div>
         </section>
         </div>
