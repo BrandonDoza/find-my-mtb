d8ce7689e9a613125a1d3d9576319735914e3a61
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-06-04 17:27:00 -0600

    add api call for patch to bike detail and style favorite button

--- src/Components/BikeDetail/BikeDetail.jsx
+++ src/Components/BikeDetail/BikeDetail.jsx
@@ -1,6 +1,6 @@
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { fetchSingleBike } from "../../apiCalls";
+import { fetchSingleBike, updateFavorite } from "../../apiCalls";
 import "./BikeDetail.css"
 
 export default function BikeDetail() {
@@ -16,7 +16,7 @@ export default function BikeDetail() {
         async function loadBike() {
             try {
                 const fetchedBike = await fetchSingleBike(id)
-                console.log('fetch', fetchedBike)
+                console.log('fetch', fetchedBike.bike)
                 setBike(fetchedBike.bike)
             }
             catch(error) {
@@ -24,18 +24,34 @@ export default function BikeDetail() {
             }
         }
         loadBike()
-    }, [])
+    }, [id])
+
+    async function toggleFavorite() {
+        try {
+            const updatedBike = await updateFavorite(id)
+            console.log(updatedBike.bike, 'update')
+            setBike(updatedBike.bike)
+        }
+        catch(error) {
+            console.log(error)
+        }
+    }
 
     if (!bike) {
         return <div>Loading...</div>;
     }
     const {make, model, suspensionTravel, wheelSize, description, imageUrl,
-        ridingStyle, skillLevel, terrain, price} = bike
+        ridingStyle, skillLevel, terrain, price, favorite} = bike
     return (
         <div className="bike-detail">
-            <Link className="close-button" onClick={handleBack}>
+            <div className="button-nav">
+            <Link  onClick={handleBack}>
             <box-icon color="#0662a0" name='window-close'size="lg"></box-icon>
             </Link>
+            <Link onClick={toggleFavorite}>
+            <box-icon name='heart'size='lg' color={favorite ? "red" :  "#0662a0"}></box-icon>
+            </Link>
+            </div>
             <h1 className="bike-make">{make}</h1>
             <h2 className="bike-model">{model}</h2>
             <h3 className="bike-stats">{`Suspension Travel: ${suspensionTravel}`}</h3>
