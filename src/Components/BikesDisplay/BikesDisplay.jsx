359fcdfd7b2ffba482847a0d03b93776397f35a3
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-06-05 13:58:17 -0600

    add home button when error is encountered

--- src/Components/BikesDisplay/BikesDisplay.jsx
+++ src/Components/BikesDisplay/BikesDisplay.jsx
@@ -1,6 +1,6 @@
 import BikeCard from "../BikeCard/BikeCard"
 import "./BikesDisplay.css"
-import { NavLink } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
 import PropTypes from "prop-types"
 
 export default function AllBikes({allBikes, error}) {
@@ -21,7 +21,12 @@ export default function AllBikes({allBikes, error}) {
     })
 
     if (error) {
-        return <div className="all-bikes-error">{`There was a problem loading the bikes, please try again later ${error}`}</div>
+        return <div className="all-bikes-error">
+            <div>{`There was a problem loading the bikes, please try again later ${error}`}</div>
+            <Link to="/">
+            <button className='home-button'>Send Me Home</button>
+        </Link>
+            </div>
     } else {
     return (
         <div className="bikes-display">
