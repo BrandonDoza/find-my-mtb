e8d756536764c3adcf68b97b6a377f97f1913514
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-06-07 08:52:09 -0600

    clean-up BikeDisplay jsx/css for readability

--- src/Components/BikesDisplay/BikesDisplay.jsx
+++ src/Components/BikesDisplay/BikesDisplay.jsx
@@ -1,42 +1,38 @@
-import BikeCard from "../BikeCard/BikeCard"
-import "./BikesDisplay.css"
-import { NavLink, Link } from "react-router-dom"
-import PropTypes from "prop-types"
+import BikeCard from "../BikeCard/BikeCard";
+import "./BikesDisplay.css";
+import { NavLink, Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-export default function AllBikes({allBikes, error}) {
-    console.log(allBikes, 'allbikes from bikesdisplay')
-    const bikesToDisplay = allBikes.map(bike => {
-        return (
-            <NavLink to={`/bikes/${bike.id}`} key={bike.id} className="link">
-            <BikeCard
-            id={bike.id}
-            make={bike.make}
-            model={bike.model}
-            img={bike.imageUrl}
-            suspensionTravel={bike.suspensionTravel}
-            price={bike.price}
-             />
-            </NavLink>
-        )
-    })
-
-    if (error) {
-        return <div className="all-bikes-error">
-            <div>{`There was a problem loading the bikes, please try again later ${error}`}</div>
-            <Link to="/">
-            <button className='home-button'>Send Me Home</button>
-        </Link>
-            </div>
-    } else {
+export default function BikesDisplay({ allBikes, error }) {
+  const bikesToDisplay = allBikes.map((bike) => {
     return (
-        <div className="bikes-display">
-            {bikesToDisplay}
-        </div>
-    )
+      <NavLink to={`/bikes/${bike.id}`} key={bike.id} className="link">
+        <BikeCard
+          id={bike.id}
+          make={bike.make}
+          model={bike.model}
+          img={bike.imageUrl}
+          suspensionTravel={bike.suspensionTravel}
+          price={bike.price}
+        />
+      </NavLink>
+    );
+  });
+
+  if (error) {
+    return (
+      <div className="all-bikes-error">
+        <div>{`There was a problem loading the bikes, please try again later ${error}`}</div>
+        <Link to="/">
+          <button className="home-button">Send Me Home</button>
+        </Link>
+      </div>
+    );
+  } else {
+    return <div className="bikes-display">{bikesToDisplay}</div>;
+  }
 }
 
-AllBikes.propTypes = { 
-    allBikes: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.object)).isRequired
-}
-}
-
+BikesDisplay.propTypes = {
+  allBikes: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.object)).isRequired,
+};
