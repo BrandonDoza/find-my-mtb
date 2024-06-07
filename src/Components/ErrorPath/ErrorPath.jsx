94824d9585cc5c30409034c8dc55c2483e868ffd
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-06-07 08:53:07 -0600

    clean-up ErrorPath jsx/css for readability

--- src/Components/ErrorPath/ErrorPath.jsx
+++ src/Components/ErrorPath/ErrorPath.jsx
@@ -1,15 +1,15 @@
-import './ErrorPath.css'
-import { Link } from 'react-router-dom'
+import "./ErrorPath.css";
+import { Link } from "react-router-dom";
 
 export default function ErrorPath() {
-    return (
-        <div className='error-path'>
-        <h1>Error, Page Not Found</h1>
-        <h2>The Page or Bike You Are Looking For Does Not Exist</h2>
-        <p>Please Enter A Valid URL</p>
-        <Link to="/">
-            <button className='home-button'>Send Me Home</button>
-        </Link>
-        </div>
-    )
+  return (
+    <div className="error-path">
+      <h1>Error, Page Not Found</h1>
+      <h2>The Page or Bike You Are Looking For Does Not Exist</h2>
+      <p>Please Enter A Valid URL</p>
+      <Link to="/">
+        <button className="home-button">Send Me Home</button>
+      </Link>
+    </div>
+  );
 }
