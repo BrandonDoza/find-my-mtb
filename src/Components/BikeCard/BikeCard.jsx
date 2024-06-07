af768c7dcad68781e17d3a551454a6a71f9792cb
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-06-07 08:43:54 -0600

    clean-up BikeCard jsx/css for readability

--- src/Components/BikeCard/BikeCard.jsx
+++ src/Components/BikeCard/BikeCard.jsx
@@ -1,22 +1,28 @@
-import "./BikeCard.css"
-import PropTypes from "prop-types"
+import "./BikeCard.css";
+import PropTypes from "prop-types";
 
-export default function BikeCard({make, model, suspensionTravel, price, img}) {
-    return (
-        <div className="bike-card">
-            <h1>{make}</h1>
-            <h2>{model}</h2>
-            <img src={img} alt={`picture of a ${make} ${model}`}/>
-            <p>{`Suspension Travel : ${suspensionTravel}mm`}</p>
-            <p>{`Price : $${price}`}</p>
-        </div>
-    )
+export default function BikeCard({
+  make,
+  model,
+  suspensionTravel,
+  price,
+  img,
+}) {
+  return (
+    <div className="bike-card">
+      <h1>{make}</h1>
+      <h2>{model}</h2>
+      <img src={img} alt={`picture of a ${make} ${model}`} />
+      <p>{`Suspension Travel : ${suspensionTravel}mm`}</p>
+      <p>{`Price : $${price}`}</p>
+    </div>
+  );
 }
 
 BikeCard.propTypes = {
-    make: PropTypes.string.isRequired,
-    model: PropTypes.string.isRequired,
-    suspensionTravel: PropTypes.number.isRequired,
-    price: PropTypes.number.isRequired,
-    img: PropTypes.string.isRequired
-}
+  make: PropTypes.string.isRequired,
+  model: PropTypes.string.isRequired,
+  suspensionTravel: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  img: PropTypes.string.isRequired,
+};
